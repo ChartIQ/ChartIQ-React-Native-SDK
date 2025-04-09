8c6620eaf850d7bce7dd6dbb5af10f3b786d1ac1
BogdanKushch <bogdan.kusch@codeit.pro>
2025-04-09 16:13:07 +0200

    fix android run & update packages

--- example/android/app/src/main/java/com/chartiqwrapperexample/MainApplication.kt
+++ example/android/app/src/main/java/com/chartiqwrapperexample/MainApplication.kt
@@ -30,7 +30,7 @@ class MainApplication : Application(), ReactApplication {
             return PackageList(this).packages
           }
 
-          override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+          override fun getJSMainModuleName(): String = "index"
 
           override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
@@ -45,9 +45,9 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
-    if (!BuildConfig.REACT_NATIVE_UNSTABLE_USE_RUNTIME_SCHEDULER_ALWAYS) {
-      ReactFeatureFlags.unstable_useRuntimeSchedulerAlways = false
-    }
+//    if (!BuildConfig.REACT_NATIVE_UNSTABLE_USE_RUNTIME_SCHEDULER_ALWAYS) {
+//      ReactFeatureFlags.unstable_useRuntimeSchedulerAlways = false
+//    }
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
